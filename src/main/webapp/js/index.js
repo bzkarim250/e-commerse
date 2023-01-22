4f121514c43e747eec7bcc66ca0b29adeae5b60d
bzkarim250 <bzkarim250@gmail.com>
2023-01-22 17:30:48 +0200

    improved cart qty

--- src/main/webapp/js/index.js
+++ src/main/webapp/js/index.js
@@ -10,27 +10,36 @@ shop.addEventListener('click',e=>{
 //add to cart
 let carts=[];
 let count=0;
+let qty=0;
 const cartButton=document.querySelectorAll('.add-to-cart');
 cartButton.forEach((button)=>{
   button.addEventListener('click',e=>{
     e.preventDefault();
-    count++;
-    cart.innerHTML=`
-    <i class="fa fa-shopping-cart"></i><span>${count}</span>
-    `;
     let button = e.target;
     let title = button.previousElementSibling.previousElementSibling.textContent;
     let price = button.previousElementSibling.textContent;
     let image = button.parentNode.firstChild.src;
+    let qty=1;
     
 
     let product = {
       title: title,
       price: price,
-      image: image
+      image: image,
+      qty:qty
     };
+    let index=carts.findIndex(p=>p.title===title);
+    if(index===-1){
+      carts.push(product);
+      count++;
+    }else{
+      carts[index].qty+=1;
+      count++;
+    }
 
-    carts.push(product);
+    cart.innerHTML=`
+    <i class="fa fa-shopping-cart"></i><span>${count}</span>
+    `;
     console.log(carts);
 
 
